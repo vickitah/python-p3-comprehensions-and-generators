3c9bc5ab2fc055990eec40659fb4a920c424ac4b
victor <victor.mutuma@student.moringaschool.com>
2025-05-11 17:10:38 +0300

    save completed changes

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # Use a list comprehension to filter even numbers
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    # Use a list comprehension to add an exclamation mark to each sentence
+    return [sentence + "!" for sentence in sentence_list]
